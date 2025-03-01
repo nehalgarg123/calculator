4373c6f04952be2fcd9fd1ccd2edcc9112dc6be9
NEHAL GARG <gargn5197@gmail.com>
2025-03-01 15:06:29 +0530

    commit

---
+++ calculator.js
@@ -0,0 +1,24 @@
+/*
+let displayanswer = document.getElementById("input");
+const btns = document.querySelectorAll(".buttons");
+
+let string = "";
+let arr = Array.from(btns);
+arr.forEach((btns) => {
+  btns.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+      input.value = string;
+    } else if (e.target.innerHTML == "Ac") {
+      string = "";
+      input.value = string;
+    } else if (e.target.innerHTML == "C") {
+      String = string.substring(0, string.length - 1);
+      input.value = string;
+    } else {
+      string += e.target.innerHTML;
+      input.value = string;
+    }
+  });
+});
+*/
